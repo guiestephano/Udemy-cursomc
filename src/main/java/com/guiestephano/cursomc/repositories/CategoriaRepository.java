014e70c356fccceae7fb53619af06319468e0016
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2023-10-04 20:03:56 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/guiestephano/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.guiestephano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guiestephano.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
